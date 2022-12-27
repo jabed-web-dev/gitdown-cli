5d6a52883b95b8240efd8101fd126bd7ef3c1341
Jabed Hossain <jabedmd837@gmail.com>
2022-12-27 14:39:59 +0600

    update download progressbar

--- src/index.js
+++ src/index.js
@@ -14,23 +14,20 @@ async function gitDown(baseurl, basedir, dirPath) {
             prePath && (basedir = resolve(prePath))
             _rootDir = rootDir
         }
-
-        let progress = ['|', '/', '—', '\\'], i = 0, logStr = `Receiving files: \x1B[0;33m0/0\x1B[1;0m || Size:\x1B[0;33m 0/0\x1B[1;0m \x1B[?25l`;
-        let logProgress = setInterval(() => {            
-            process.stderr.cursorTo(0);
-            process.stderr.write(`\x1B[0;36m${progress[i++]}\x1B[1;0m ${logStr}`);
-            process.stderr.clearLine(1);
-            i = i >= progress.length ? 0 : i;
-        }, 150);
+        let progress = ['|', '/', '—', '\\'], i = 0;
 
         downloadFiles(baseurl, basedir, _rootDir, dirPath, (totalFiles, totalDownloadFiles, size, done, repo) => {
-            logStr = `Receiving files: \x1B[0;33m${totalFiles}/${totalDownloadFiles}\x1B[1;0m || Size:\x1B[0;33m ${formatSize(size, repo)}\x1B[1;0m \x1B[?25l`
+            let logStr = `\x1B[0;36m${progress[i++]}\x1B[1;0m Receiving files: \x1B[0;33m${totalFiles}/${totalDownloadFiles}\x1B[1;0m Size:\x1B[0;33m ${formatSize(size, repo)}\x1B[1;0m`
+            i = i >= progress.length ? 0 : i;
             
+            process.stderr.cursorTo(0);
+            process.stderr.write(logStr);
+            process.stderr.clearLine(1);
             if (done && totalFiles === totalDownloadFiles) {
-                clearInterval(logProgress)
                 console.log(`\n✅ \x1B[0;32mDownload completed\x1B[1;0m`)
             }
         })
+
     } catch (err) {
         console.log(`${err.name}: ${err.message}`)
     }
